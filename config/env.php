efdb8bcc3cb0b7351a5c453c68e4a693d1f43286
lsudre <lsudre@student.42.fr>
2019-07-30 00:29:38 +0200

    update

--- config/env.php
+++ config/env.php
@@ -1,5 +1,5 @@
 <?php
-define("DBUSER", "louis");
-define("DBPWD", "12Fodaqi");
-define("DBHOSTNAME", "mysql:host=127.0.0.1;port=3307;dbname=dev");
+define("DBUSER", "web");
+define("DBPWD", "12Fodaqi$");
+define("DBHOSTNAME", "mysql:host=127.0.0.1;port=3306;dbname=production");
 ?>
